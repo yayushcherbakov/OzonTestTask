8369643312d384b33bafc79e1b7e9cba775409ef
yayushcherbakov <91913462+yayushcherbakov@users.noreply.github.com>
2024-06-23 18:59:47 +0300

    add interceptor

---
+++ RequestProcessingService.Presentation/Constants/LoggerMessagesConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace RequestProcessingService.Presentation.Constants;
+
+internal static class LoggerMessagesConstants
+{
+    public const string ExceptionMessage =
+        "An exception has been thrown: {ExceptionMessage}";
+
+    public const string NotFoundExceptions =
+        "Report with identifier not found";
+}
---
+++ RequestProcessingService.Presentation/Interceptors/ErrorHandlerInterceptor.cs
@@ -0,0 +1,48 @@
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+using RequestProcessingService.BusinessLogic.Exceptions;
+using RequestProcessingService.Presentation.Constants;
+
+namespace RequestProcessingService.Presentation.Interceptors;
+
+public class ErrorHandlerInterceptor : Interceptor
+{
+    private readonly ILogger<ErrorHandlerInterceptor> _logger;
+
+    public ErrorHandlerInterceptor(ILogger<ErrorHandlerInterceptor> logger)
+    {
+        _logger = logger;
+    }
+
+    public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>
+    (
+        TRequest request,
+        ServerCallContext context,
+        UnaryServerMethod<TRequest, TResponse> continuation
+    )
+    {
+        try
+        {
+            return await continuation(request, context);
+        }
+        catch (NotFoundException ex)
+        {
+            _logger.LogInformation
+            (
+                LoggerMessagesConstants.NotFoundExceptions
+            );
+
+            throw new NotFoundException(LoggerMessagesConstants.NotFoundExceptions);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogInformation
+            (
+                LoggerMessagesConstants.ExceptionMessage,
+                ex.Message
+            );
+
+            throw new RpcException(new Status(StatusCode.Internal, ex.Message));
+        }
+    }
+}
