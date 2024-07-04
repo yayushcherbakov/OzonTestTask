7428343d04ef916ce27c1fbccd33af787cfdc3ac
yayushcherbakov <91913462+yayushcherbakov@users.noreply.github.com>
2024-07-04 14:50:57 +0300

    Comments on the completion of the task.

--- ReportRequestEventGenerator/Kafka/KafkaPublisher.cs
+++ ReportRequestEventGenerator/Kafka/KafkaPublisher.cs
@@ -44,14 +44,21 @@ internal sealed class KafkaPublisher<TKey, TValue> : IDisposable
 
     public async Task Publish(IEnumerable<(TKey key, TValue value)> messages, CancellationToken token)
     {
+        // Индикатор завершения задачи публикации сообщений.
         var completionSource = new TaskCompletionSource<bool>();
+
+        // Регистрация отмены задачи, если токен сработает.
         await using var registration = token.Register(() => completionSource.TrySetCanceled(token));
 
+        // Количество сообщений в очереди публикации сообщений.
         int messagesInQueue = 1;
 
         foreach (var (key, value) in messages)
         {
+            // Потокобезопасно увеличивает счетчик на единицу.
             Interlocked.Increment(ref messagesInQueue);
+
+            // Асинхронная публикация сообщения без ожидания выполнения.
             var awaiter = _producer
                 .ProduceAsync(
                     _topic,
@@ -60,6 +67,7 @@ internal sealed class KafkaPublisher<TKey, TValue> : IDisposable
                 .ConfigureAwait(false)
                 .GetAwaiter();
 
+            // Здесь мы подписываемся на событие завершения задачи.
             awaiter.OnCompleted(
                 () =>
                 {
@@ -67,20 +75,32 @@ internal sealed class KafkaPublisher<TKey, TValue> : IDisposable
                     {
                         awaiter.GetResult();
 
-                        // The counter reaches zero only if the loop has ended.
+                        // Если счетчик достигает нуля, вызывается completionSource.SetResult(true),
+                        // сигнализируя об успешном завершении всех операций.
                         if (Interlocked.Decrement(ref messagesInQueue) == 0)
+                        {
                             completionSource.SetResult(true);
+                        }
                     }
                     catch (Exception exception)
                     {
+                        // В случае исключения задача завершается с ошибкой.
                         completionSource.TrySetException(exception);
                     }
                 });
         }
 
+        // Проверка вызывается еще раз для покрытия случая,
+        // когда все сообщения были отправлены до попаданию в эту строчку кода.
         if (Interlocked.Decrement(ref messagesInQueue) == 0)
+        {
             completionSource.TrySetResult(true);
+        }
 
+
+        // Задача будет выполнена после того, как все сообщения будут опубликованы, и messagesInQueue станет 
+        // равным нулю, что означает завершение всех асинхронных операций по публикации сообщений, или когда возникнет
+        // исключения при отправке сообщения. (вызван метод SetResult, TrySetResult, TrySetException).
         await completionSource.Task;
     }
 }
